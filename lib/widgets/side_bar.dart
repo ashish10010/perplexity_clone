8b688c184532c11e131d17f6d1197845dddd4343
Ashish Kunwar <thaladipesh89@gmail.com>
2024-12-29 23:35:42 +0545

    sidebar ? isCollapsed : expanded

--- lib/widgets/side_bar.dart
+++ lib/widgets/side_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:perplexity_clone/theme/colors.dart';
+import 'package:perplexity_clone/widgets/side_bar_button.dart';
 
 class SideBar extends StatefulWidget {
   const SideBar({super.key});
@@ -14,11 +15,9 @@ class _SideBarState extends State<SideBar> {
   Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: const Duration(microseconds: 100),
-      width: isCollapsed ? 64 : 128,
+      width: isCollapsed ? 64 : 160,
       color: AppColors.sideNav,
       child: Column(
-        crossAxisAlignment:
-            isCollapsed ? CrossAxisAlignment.center : CrossAxisAlignment.start,
         children: [
           const SizedBox(
             height: 16,
@@ -26,37 +25,46 @@ class _SideBarState extends State<SideBar> {
           Icon(
             Icons.auto_awesome_mosaic,
             color: AppColors.whiteColor,
-            size: 30,
+            size: isCollapsed ? 30 : 60,
           ),
-          const SizedBox(
-            height: 24,
-          ),
-       
-          Container(
-            margin: EdgeInsets.all(14),
-            child: Icon(
-              Icons.search,
-              color: AppColors.iconGrey,
-              size: 22,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: isCollapsed
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 24,
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.add,
+                  text: "Home",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.search,
+                  text: "Search",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.language,
+                  text: "Spaces",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.auto_awesome,
+                  text: "Discover",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.cloud_outlined,
+                  text: "Library",
+                ),
+                const Spacer(),
+              ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
-            child: Icon(
-              Icons.auto_awesome,
-              color: AppColors.iconGrey,
-              size: 22,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
-            child: Icon(
-              Icons.cloud_outlined,
-              color: AppColors.iconGrey,
-              size: 22,
-            ),
-          ),
-          const Spacer(),
           GestureDetector(
             onTap: () {
               setState(() {
