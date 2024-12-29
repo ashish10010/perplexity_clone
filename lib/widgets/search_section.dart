63eed969c0a2633d6c8cc1441c46ee41a192b00c
Ashish Kunwar <thaladipesh89@gmail.com>
2024-12-29 23:19:04 +0545

    homepageUI

--- lib/widgets/search_section.dart
+++ lib/widgets/search_section.dart
@@ -5,6 +5,11 @@ class SearchSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      width: 64,
+      child: Column(
+        
+      ),
+    );
   }
 }
--- lib/widgets/side_bar.dart
+++ lib/widgets/side_bar.dart
@@ -1,14 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:perplexity_clone/theme/colors.dart';
 
-class SideBar extends StatelessWidget {
+class SideBar extends StatefulWidget {
   const SideBar({super.key});
 
   @override
+  State<SideBar> createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  bool isCollapsed = true;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 64,
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 100),
+      width: isCollapsed ? 64 : 128,
       color: AppColors.sideNav,
+      child: Column(
+        crossAxisAlignment:
+            isCollapsed ? CrossAxisAlignment.center : CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 16,
+          ),
+          Icon(
+            Icons.auto_awesome_mosaic,
+            color: AppColors.whiteColor,
+            size: 30,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+       
+          Container(
+            margin: EdgeInsets.all(14),
+            child: Icon(
+              Icons.search,
+              color: AppColors.iconGrey,
+              size: 22,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+            child: Icon(
+              Icons.auto_awesome,
+              color: AppColors.iconGrey,
+              size: 22,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+            child: Icon(
+              Icons.cloud_outlined,
+              color: AppColors.iconGrey,
+              size: 22,
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isCollapsed = !isCollapsed;
+              });
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 100),
+              margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+              child: Icon(
+                isCollapsed
+                    ? Icons.keyboard_arrow_right
+                    : Icons.keyboard_arrow_left,
+                color: AppColors.iconGrey,
+                size: 22,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
     );
   }
 }
