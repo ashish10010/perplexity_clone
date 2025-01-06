bfb58da4bd16fedcb29b92f24c1c67f2dbb90486
Ashish Kunwar <thaladipesh89@gmail.com>
2025-01-06 21:05:55 +0545

    singleton class so the class never dies

--- lib/services/chat_web_service.dart
+++ lib/services/chat_web_service.dart
@@ -3,7 +3,13 @@ import 'dart:convert';
 import 'package:web_socket_client/web_socket_client.dart';
 
 class ChatWebService {
+  
+  static final _instance = ChatWebService._internal();
   WebSocket? _socket;
+
+  factory ChatWebService() => _instance;
+
+  ChatWebService._internal();
 
   void connect() {
     _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
